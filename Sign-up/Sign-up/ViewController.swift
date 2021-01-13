19fd6a39a4dc088aa13074ef65d37c4a43e867da
souravrana123 <souravrana61@gmail.com>
2021-01-13 10:37:33 +0530

    added Sign-up

---
+++ Sign-up/Sign-up/ViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  Sign-up
+//
+//  Created by Drish on 12/01/21.
+//  Copyright © 2021 Drish. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController,  UITextFieldDelegate {
+    
+    @IBOutlet weak var signupLb: UILabel!
+    
+    @IBOutlet weak var usernameTf: UITextField!
+    @IBOutlet weak var passwordTf: UITextField!
+    @IBOutlet weak var nameTf: UITextField!
+    @IBOutlet weak var phonenumberTf: UITextField!
+    @IBOutlet weak var submitBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.usernameTf.delegate = self
+        self.passwordTf.delegate = self
+        self.nameTf.delegate = self
+        self.phonenumberTf.delegate = self
+        
+        self.usernameTf.returnKeyType = UIReturnKeyType.next
+        self.passwordTf.returnKeyType = UIReturnKeyType.continue
+        self.nameTf.returnKeyType = UIReturnKeyType.emergencyCall
+        self.phonenumberTf.returnKeyType = UIReturnKeyType.done
+        //    self.submitBtn.delegate = self button does not have delegate
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+       //if desired
+        return true
+    }
+    
+    @IBAction func submitBtnTapped(_ sender: Any)
+    {
+        print("User-name: \(usernameTf.text!)\npassword: \(passwordTf.text!)name: \(nameTf.text!)\nphone-number: \(phonenumberTf.text!)")
+    }
+    
+    
+}
+
---
+++ Sign-up/Sign-up/tableVC.swift
@@ -0,0 +1,9 @@
+//
+//  tableVC.swift
+//  Sign-up
+//
+//  Created by Drish on 13/01/21.
+//  Copyright © 2021 Drish. All rights reserved.
+//
+
+import Foundation
